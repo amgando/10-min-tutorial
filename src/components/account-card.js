a44cae108ede58653f8a8c21e8c18da81bd71778
Tanner Welsh <tannerwelsh@gmail.com>
2020-04-03 10:20:20 -0600

    update cards component
    & remove images

--- src/components/account-card.js
+++ src/components/account-card.js
@@ -1,24 +1,11 @@
 import React, { useEffect, useState } from "react"
 import { Card, Icon, Image, Transition } from "semantic-ui-react"
 
-import bankImage from "../images/account-bank.jpg"
-import aliceImage from "../images/account-alice.jpg"
-import bobImage from "../images/account-bob.jpg"
-import carolImage from "../images/account-carol.jpg"
-
 const iconForType = type =>
   ({
     bank: "university",
     person: "user circle outline",
   }[type])
-
-const imageForId = id =>
-  ({
-    bank: bankImage,
-    alice: aliceImage,
-    bob: bobImage,
-    carol: carolImage,
-  }[id])
 
 const AccountCard = ({ id, name, balance, type }) => {
   const [toggled, setToggled] = useState(true)
@@ -28,20 +15,19 @@ const AccountCard = ({ id, name, balance, type }) => {
 
   return (
     <Card>
-      <Image fluid src={imageForId(id)} />
       <Card.Content>
-        <Card.Header>{name}</Card.Header>
-        <Card.Meta>
-          <Icon name={iconForType(type)} />
-          {type === "person" ? "User" : "Bank"} Account
-        </Card.Meta>
+        <Transition animation="glow" visible={toggled}>
+          <Card.Header>
+            <Icon name="dot circle outline" />
+            {balance}
+          </Card.Header>
+        </Transition>
+        <Card.Meta>{name}</Card.Meta>
       </Card.Content>
-      <Transition animation="glow" visible={toggled}>
-        <Card.Content extra>
-          <Icon name="dot circle outline" />
-          {balance}
-        </Card.Content>
-      </Transition>
+      <Card.Content extra>
+        <Icon name={iconForType(type)} />
+        {type === "person" ? "User" : "Bank"} Account
+      </Card.Content>
     </Card>
   )
 }
